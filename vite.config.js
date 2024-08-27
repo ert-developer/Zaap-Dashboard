8bd70d7b2d1ed19f6d729ac7803c8c0c78127fd1
sankeerth-g7 <sankeertherra01@gmail.com>
2024-08-27 15:03:47 +0530

    vite port

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.PORT || 4000
-  },
-})
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      // Parse VITE_PORT from environment variables or default to 4000
+      port: parseInt(env.VITE_PORT) || 4000,
+    },
+  }
+});
