e68ec3a32d5e8ced48271665813dc24ed16ff1a5
sankeerth-g7 <sankeertherra01@gmail.com>
2024-09-04 12:33:40 +0530

    report jobs

--- src/components/reports/reports-screen.jsx
+++ src/components/reports/reports-screen.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import "./reports-styles.css";
 
 const ReportsScreen = ({ reportDetails }) => {
+  console.log(reportDetails);
   return (
     <div className="reports-container">
       <h2 className="reports-heading">Reports</h2>
       {reportDetails.map((report, index) => (
         <div key={index} className="report-card">
           <div className="report-header">
-            <p className="report-name">
-              <strong>Name:</strong> {report.reporterName}
-            </p>
-            <p className="report-email">
-              <strong>Email:</strong> {report.reporterEmail}
-            </p>
+            <div className="report-info">
+              <p className="report-name">
+                <strong>Name:</strong> {report.reporterName}
+              </p>
+              <p className="report-email">
+                <strong>Email:</strong> {report.reporterEmail}
+              </p>
+              <p className="report-title">
+                <strong>Job Title:</strong> {report.jobTitle}
+              </p>
+              <p className="report-description">
+                <strong>Job Description:</strong> {report.jobDescription}
+              </p>
+            </div>
+            <div className="report-actions">
+              <button className="report-button delete-button">Delete Job</button>
+              <button className="report-button view-button">Remove Report</button>
+            </div>
           </div>
           <p>
             <strong>Report Type:</strong>{" "}
