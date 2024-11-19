c31d1ff4dc435c927c265a6abda026cbded82acf
Aditya <115411253+Aditya-dnvks@users.noreply.github.com>
2024-11-19 20:15:24 +0530

    Update customer-payment-screen.jsx

--- src/components/customerpayment/customer-payment-screen.jsx
+++ src/components/customerpayment/customer-payment-screen.jsx
@@ -21,7 +21,7 @@ const CustomerPaymentScreen = () => {
   useEffect(() => {
     const getDataFromCustomerDev = async () => {
       const customersPaymentData = await getDocs(
-        collection(fireStoreDB, envConfig.Payment)
+        collection(fireStoreDB, envConfig.Payments)
       );
       const result = customersPaymentData.docs.map((doc) => {
         const createdTimestamp = doc.data().createdOn;
